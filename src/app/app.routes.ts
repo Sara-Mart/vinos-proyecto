a4a6084b6da4fb92a9817550f64cce10344e122b
SaraMart <sara.martinez965@unir.net>
2026-01-12 23:38:11 +0100

    Menu de navegación creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Inicio } from './pages/inicio/inicio';
+import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
+import { Component } from '@angular/core';
+import { Productos } from './pages/productos/productos';
+/*Rutas de navegación dentro del proyecto*/
+export const routes: Routes = [
+    {path:'', component: Inicio},
+    {path:'sobre-nosotros', component: SobreNosotros},
+    {path:'productos', component:Productos},
+    {path:'**', redirectTo:''},
+];
